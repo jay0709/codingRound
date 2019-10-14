376d39a75f1afae7dfcf773144eeb47afbeed3fb
Jay <Jayabrata@Mac-G8LWG3QK>
2019-10-14 21:36:36 +0530

    1st Commit - SignInTest Class

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,41 +1,70 @@
-import com.sun.javafx.PlatformUtil;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.sun.javafx.PlatformUtil;
+
+@SuppressWarnings("restriction")
 public class SignInTest {
+	
 
+    @FindBy(id="modal_window") 
+    WebElement frame_signInPopUp;
+    
+    @FindBy(linkText= "Your trips")
+    WebElement link_YourTrips;
+    
+    @FindBy(id="SignIn")
+    WebElement btn_SignIn;
+    
+    @FindBy(id="signInButton")
+    WebElement btn_FrameSignIn;
+    
+    @FindBy(id="errors1")
+    WebElement msg_ErrorSignIn;
+    
     WebDriver driver = new ChromeDriver();
-
+    
+    
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
         setDriverPath();
-
+        
         driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+        driver.manage().window().maximize();
+        PageFactory.initElements(driver, this);
+        
+        link_YourTrips.click();
+        explicitWaitElementVisibility(btn_SignIn);
+        btn_SignIn.click();
+        explicitWaitElementVisibility(frame_signInPopUp);
+        switchToFrame(frame_signInPopUp);
+        explicitWaitElementVisibility(btn_FrameSignIn);
+        btn_FrameSignIn.click();
 
-        driver.findElement(By.linkText("Your trips")).click();
-        driver.findElement(By.id("SignIn")).click();
-
-        driver.findElement(By.id("signInButton")).click();
-
-        String errors1 = driver.findElement(By.id("errors1")).getText();
-        Assert.assertTrue(errors1.contains("There were errors in your submission"));
+        String errors = msg_ErrorSignIn.getText();
+        Assert.assertTrue(errors.contains("There were errors in your submission"));
         driver.quit();
     }
+    
 
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+	private void explicitWaitElementVisibility(WebElement element) {
+    	WebDriverWait wait = new WebDriverWait(driver, 10000);
+    	wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+    
+    private void switchToFrame(WebElement frame) {
+    	driver.switchTo().frame(frame);
     }
 
-    private void setDriverPath() {
+	private void setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
